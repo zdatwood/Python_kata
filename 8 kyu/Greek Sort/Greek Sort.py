6f0df5d7c7adc41fee2858c03c541167bdeb46ae
zdatwood <zdatwood@protonmail.com>
2021-08-18 17:19:24 -0400

    soution for Greek Sort

--- 8 kyu/Greek Sort/Greek Sort.py
+++ 8 kyu/Greek Sort/Greek Sort.py
@@ -1,6 +1,20 @@
+def greek_comparator(lhs, rhs):
+    for i in range(len(greek_alphabet)):
+        if greek_alphabet[i] == lhs:
+            x = i
+        if greek_alphabet[i] == rhs:
+            y = i
+    # the tuple greek_alphabet is defined in the global namespace
+    return (x - y)
+
+
 greek_alphabet = (
     'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta',
     'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu',
     'nu', 'xi', 'omicron', 'pi', 'rho', 'sigma',
     'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega')
 
+
+print(greek_comparator('alpha', 'beta'))
+print(greek_comparator('psi', 'psi'))
+print(greek_comparator('upsilon', 'rho'))
